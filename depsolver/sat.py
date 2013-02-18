8755e78f92400660ad6f97749eeaee5eaf06c1e0
David Cournapeau <cournape@gmail.com>
2013-02-18 12:30:56 +0000

    FEAT: add brute-force SAT solver.

---
+++ depsolver/sat.py
@@ -0,0 +1,133 @@
+from collections import OrderedDict
+
+class Literal(object):
+    """Creates a simple Literal to be used within clauses.
+
+    Parameters
+    ----------
+    name: str
+        Name of the literal (must consists of alphanumerical characters only)
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "L('%s')" % self.name
+
+    def __or__(self, other):
+        return Clause([self, other])
+
+    def __invert__(self):
+        return Not(self.name)
+
+    def evaluate(self, values):
+        if not self.name in values:
+            raise ValueError("literal %s is undefined" % self.name)
+        else:
+            return values[self.name]
+
+class Not(Literal):
+    def __repr__(self):
+        return "L('~%s')" % self.name
+
+    def evaluate(self, values):
+        return not super(Not, self).evaluate(values)
+
+class Clause(object):
+    @classmethod
+    def from_string(cls, clause_string):
+        literals = []
+        for literal_string in clause_string.split("|"):
+            literal_string = literal_string.strip()
+            if literal_string.startswith("~"):
+                if len(literal_string) < 2:
+                    raise ValueError("Invalid literal string: %r" % literal_string)
+                else:
+                    literals.append(Not(literal_string[1:]))
+            else:
+                literals.append(Literal(literal_string))
+        return cls(literals)
+
+    def __init__(self, literals):
+        self._literals = literals
+        self._name_to_literal = dict((literal.name, literal) for literal in literals)
+
+        self._literals_set = set(l.name for l in literals)
+
+    @property
+    def literals(self):
+        return self._literals_set
+
+    def evaluate(self, values):
+        ret = False
+        for literal in self._literals:
+            if not literal.name in values:
+                raise ValueError("literal %s value is undefined" % (literal.name,))
+            else:
+                ret |= literal.evaluate(values)
+        return ret
+
+    def __or__(self, other):
+        if isinstance(other, Clause):
+            return Clause(self._literals + other._literals)
+        elif isinstance(other, Literal):
+            return Clause(self._literals + [other])
+        else:
+            raise TypeError("unsupported type %s" % type(other))
+
+    def __repr__(self):
+        def _simple_literal(l):
+            return "~%s" % l.name if isinstance(l, Not) else l.name
+        return "C({})".format(" | ".join(_simple_literal(l) for l in self._literals))
+
+def _find_unset_variable(clauses, variables):
+    for clause in clauses:
+        for l in clause.literals:
+            if not l in variables:
+                return l
+    return None
+
+def _try_evaluate(clauses, variables):
+    unset = _find_unset_variable(clauses, variables)
+    if unset is not None:
+        variables[unset] = True
+        is_satisfiable, new_variables = _try_evaluate(clauses, variables)
+        if is_satisfiable:
+            return is_satisfiable, new_variables
+        else:
+            variables[unset] = False
+            is_satisfiable, new_variables = _try_evaluate(clauses, variables)
+            if is_satisfiable:
+                return is_satisfiable, new_variables
+            else:
+                variables.popitem()
+                return False, variables
+    else:
+        for clause in clauses:
+            if not clause.evaluate(variables):
+                return False, variables
+        return True, variables
+
+def is_satisfiable(clauses, variables=None):
+    """A naive back-tracking implementation of SAT solver.
+    
+    Parameters
+    ----------
+    clauses: set
+        Set of clauses instance. The set is considered as a conjunction of
+        clauses
+    variables: mapping
+        OrderedDict containing pre-defined variables -> boolean.
+
+    Returns
+    -------
+    satisfiability: bool
+        True if the given instance is satisfiable
+    variables: mapping
+        If satisfiable, gives one found solution. The solution is a simple
+        mapping variable name -> boolean.
+    """
+    if variables is None:
+        variables = OrderedDict()
+    result, result_variables = _try_evaluate(clauses, variables)
+    return result, dict(result_variables)
---
+++ depsolver/tests/test_sat.py
@@ -0,0 +1,137 @@
+import collections
+import unittest
+
+from depsolver.sat \
+    import \
+        Clause, Literal, is_satisfiable
+
+class TestLiteral(unittest.TestCase):
+    def test_simple(self):
+        a = Literal("a")
+        self.assertTrue(a.evaluate({"a": True}))
+        self.assertFalse(a.evaluate({"a": False}))
+
+        self.assertFalse((~a).evaluate({"a": True}))
+        self.assertTrue((~a).evaluate({"a": False}))
+
+        self.assertRaises(ValueError, lambda: a.evaluate({}))
+
+    @unittest.expectedFailure
+    def test_invalid_literal_name(self):
+        self.assertRaises(ValueError, lambda: Literal("~a"))
+
+    def test_repr(self):
+        a = Literal("a")
+        self.assertEqual(repr(a), "L('a')")
+        self.assertEqual(repr(~a), "L('~a')")
+
+    def test_clause(self):
+        a = Literal("a")
+        b = Literal("b")
+
+        clause = a | b
+        self.assertTrue(clause.evaluate({"a": True, "b": True}))
+        self.assertTrue(clause.evaluate({"a": True, "b": False}))
+        self.assertTrue(clause.evaluate({"a": False, "b": True}))
+        self.assertFalse(clause.evaluate({"a": False, "b": False}))
+
+class TestClause(unittest.TestCase):
+    def test_simple(self):
+        a = Literal("a")
+        b = Literal("b")
+        clause = Clause([a, b])
+
+        self.assertEqual(clause.literals, set(["a", "b"]))
+
+    def test_create_from_string(self):
+        clause = Clause.from_string("a | b | ~c")
+
+        self.assertEqual(clause.literals, set(["a", "b", "c"]))
+        self.assertTrue(clause.evaluate({"a": False, "b": False, "c": False}))
+
+        clause = Clause.from_string("a | b | c")
+
+        self.assertEqual(clause.literals, set(["a", "b", "c"]))
+        self.assertFalse(clause.evaluate({"a": False, "b": False, "c": False}))
+
+    def test_or(self):
+        a = Literal("a")
+        b = Literal("b")
+        clause = Clause([a, b])
+        clause |= Literal("c")
+
+        self.assertEqual(clause.literals, set(["a", "b", "c"]))
+
+        a = Literal("a")
+        b = Literal("b")
+        clause = Clause([a, b])
+
+        c = Literal("c")
+        d = Literal("d")
+        clause |= Clause([c, d])
+
+        self.assertEqual(clause.literals, set(["a", "b", "c", "d"]))
+
+    def test_repr(self):
+        a = Literal("a")
+        b = Literal("b")
+        clause = Clause([a, b])
+
+        self.assertEqual(repr(clause), "C(a | b)")
+
+        a = Literal("a")
+        b = ~Literal("b")
+        clause = Clause([a, b])
+
+        self.assertEqual(repr(clause), "C(a | ~b)")
+
+    def test_evaluate(self):
+        a = Literal("a")
+        b = Literal("b")
+        clause = Clause([a, b])
+
+        self.assertTrue(clause.evaluate({"a": True, "b": True}))
+        self.assertFalse(clause.evaluate({"a": False, "b": False}))
+
+        self.assertRaises(ValueError, lambda: clause.evaluate({"a": False}))
+
+class TestSAT(unittest.TestCase):
+    def test_simple(self):
+        clause = Literal("a") | Literal("b")
+
+        res, variables = is_satisfiable(set([clause]))
+        self.assertTrue(res)
+        self.assertTrue(variables in [{"a": True, "b": True}, {"a": True, "b": False},
+                                      {"a": False, "b": True}])
+
+        clause = Literal("a") | ~Literal("b")
+
+        res, variables = is_satisfiable(set([clause]))
+        self.assertTrue(res)
+        self.assertTrue(variables in [{"a": True, "b": True}, {"a": True, "b": False},
+                                      {"a": False, "b": False}])
+
+    def test_clauses(self):
+        clause1 = Clause([Literal("a")])
+        clause2 = Clause([~Literal("a")])
+
+        res, variables = is_satisfiable(set([clause1, clause2]))
+        self.assertFalse(res)
+
+        clause1 = Clause([Literal("a"), Literal("b")])
+        clause2 = Clause([~Literal("a")])
+
+        res, variables = is_satisfiable(set([clause1, clause2]))
+        self.assertTrue(res)
+        self.assertTrue(variables, {"a": False, "b": True})
+
+    def test_existing_variables(self):
+        clause1 = Clause([Literal("a"), Literal("b")])
+        clause2 = Clause([~Literal("a")])
+
+        res, variables = is_satisfiable(set([clause1, clause2]))
+        self.assertTrue(res)
+
+        res, variables = is_satisfiable(set([clause1, clause2]),
+                collections.OrderedDict({"a": True}))
+        self.assertFalse(res)
