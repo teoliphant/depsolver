4efe9309dcd0b76adce46858ccd1f4688b82de88
David Cournapeau <cournape@gmail.com>
2013-02-18 00:59:40 +0000

    FEAT: add basic Package class to track dep/provides.

---
+++ depsolver/package.py
@@ -0,0 +1,37 @@
+import hashlib
+
+class Package(object):
+    def __init__(self, name, version, provides=None, dependencies=None):
+        """Create a new package instance.
+
+        Parameters
+        ----------
+        name: str
+            Name of the package (i.e. distribution name)
+        version: object
+            Instance of Version
+        provides: None or sequence
+            Sequence of distribution name provided. Package name always
+            considered provided
+        dependencies: None or sequence
+            Sequence of Requirements.
+        """
+        self.name = name
+        self.version = version
+
+        if provides is None:
+            self.provides = set()
+        else:
+            self.provides = set(provides)
+        self.provides.add(self.name)
+
+        if dependencies is None:
+            self.dependencies = set()
+        else:
+            self.dependencies = set(dependencies)
+
+        # FIXME: id detail should be implemented outside Package interface
+        self.id = hashlib.md5(self.name + "-" + str(self.version)).hexdigest()
+
+    def __repr__(self):
+        return "Package(%s, %s)" % (self.name, self.version)
---
+++ depsolver/tests/test_package.py
@@ -0,0 +1,27 @@
+import hashlib
+import unittest
+
+from depsolver.package \
+    import \
+        Package
+from depsolver.version \
+    import \
+        Version
+
+V = Version.from_string
+
+class TestPackage(unittest.TestCase):
+    def test_construction(self):
+        r_provides = set(["numpy"])
+        r_id = hashlib.md5("numpy-1.3.0").hexdigest()
+
+        package = Package("numpy", V("1.3.0"))
+        self.assertEqual(package.provides, r_provides)
+        self.assertEqual(package.id, r_id)
+
+        r_provides = set(["numpy", "nomkl_numpy"])
+        r_id = hashlib.md5("nomkl_numpy-1.3.0").hexdigest()
+
+        package = Package("nomkl_numpy", V("1.3.0"), provides=["numpy"])
+        self.assertEqual(package.provides, r_provides)
+        self.assertEqual(package.id, r_id)
