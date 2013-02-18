244cbf6c273a4a01855cb8edcf3fb4882d94f3a7
David Cournapeau <cournape@gmail.com>
2013-02-18 01:09:29 +0000

    FEAT: add Requirement class to mach package and requirement specs.

---
+++ depsolver/requirement.py
@@ -0,0 +1,79 @@
+from depsolver.requirement_parser \
+    import \
+        EqualSpecification, GEQSpecification, LEQSpecification, RawRequirementParser
+from depsolver.version \
+    import \
+        Version
+
+V = Version.from_string
+
+class Requirement(object):
+    def __init__(self, name, specs):
+        self.name = name
+
+        # transform GE and LE into NOT + corresponding GEQ/LEQ
+        # Take the min of GEQ, max of LEQ
+        equals = [req for req in specs if isinstance(req, EqualSpecification)]
+        if len(equals) > 1:
+            self._cannot_match = True
+            self._equal = None
+        elif len(equals) == 1:
+            self._cannot_match = False
+            self._equal = V(equals[0].version)
+        else:
+            self._cannot_match = False
+            self._equal = None
+
+        geq = [req for req in specs if isinstance(req, GEQSpecification)]
+        geq_versions = [V(g.version) for g in geq]
+        if len(geq_versions) > 0:
+            self._min_bound = max(geq_versions)
+        else:
+            self._min_bound = None
+
+        leq = [req for req in specs if isinstance(req, LEQSpecification)]
+        leq_versions = [V(l.version) for l in leq]
+        if len(leq_versions) > 0:
+            self._max_bound = min(leq_versions)
+        else:
+            self._max_bound = None
+
+    def __repr__(self):
+        r = []
+        if self._min_bound:
+            r.append("%s >= %s" % (self.name, self._min_bound))
+        if self._max_bound:
+            r.append("%s <= %s" % (self.name, self._max_bound))
+        if self._equal:
+            r.append("%s == %s" % (self.name, self._equal))
+        return ", ".join(r)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def match(self, package):
+        """Return True if the given package matches this requirement."""
+        if not self.name in package.provides:
+            return False
+        if self._cannot_match:
+            return False
+        else:
+            version = package.version
+            if self._equal:
+                if version == self._equal:
+                    return True
+                else:
+                    return False
+            else:
+                if version >= self._min_bound and version <= self._max_bound:
+                    return True
+                else:
+                    return False
+
+class RequirementParser(object):
+    def __init__(self):
+        self._parser = RawRequirementParser()
+
+    def parse(self, requirement_string):
+        for distribution_name, specs in self._parser.parse(requirement_string).iteritems():
+            yield Requirement(distribution_name, specs)
---
+++ depsolver/tests/test_requirement.py
@@ -0,0 +1,59 @@
+import unittest
+
+from depsolver.package \
+    import \
+        Package
+from depsolver.requirement \
+    import \
+        Requirement, RequirementParser
+from depsolver.requirement_parser \
+    import \
+        EqualSpecification, GEQSpecification, LEQSpecification
+from depsolver.version \
+    import \
+        Version
+
+V = Version.from_string
+
+class TestRequirementParser(unittest.TestCase):
+    def test_simple(self):
+        parser = RequirementParser()
+
+        r_requirements = [Requirement("numpy", [GEQSpecification("1.3.0")])]
+        requirements = parser.parse("numpy >= 1.3.0")
+        self.assertEqual(r_requirements, list(requirements))
+
+        r_requirements = [Requirement("numpy", [GEQSpecification("1.3.0"),
+                                                LEQSpecification("2.0.0")])]
+        requirements = parser.parse("numpy >= 1.3.0, numpy <= 2.0.0")
+        self.assertEqual(r_requirements, list(requirements))
+
+        r_requirements = [Requirement("numpy", [EqualSpecification("1.3.0")])]
+        requirements = parser.parse("numpy == 1.3.0")
+        self.assertEqual(r_requirements, list(requirements))
+
+        r_requirements = [Requirement("numpy", [EqualSpecification("1.3.0"),
+                                                EqualSpecification("1.4.0")])]
+        requirements = parser.parse("numpy == 1.3.0, numpy == 1.4.0")
+        self.assertEqual(r_requirements, list(requirements))
+
+    def test_repr(self):
+        requirement_string = "numpy >= 1.3.0, numpy <= 2.0.0"
+        parser = RequirementParser()
+        numpy_requirement = list(parser.parse(requirement_string))[0]
+
+        self.assertEqual(repr(numpy_requirement), requirement_string)
+
+    def test_match(self):
+        parser = RequirementParser()
+
+        numpy_requirement = list(parser.parse("numpy >= 1.3.0, numpy <= 2.0.0"))[0]
+        self.assertFalse(numpy_requirement.match(Package("numpy", V("1.2.0"))))
+        self.assertTrue(numpy_requirement.match(Package("numpy", V("1.4.0"))))
+        self.assertTrue(numpy_requirement.match(Package("numpy", V("1.5.0"))))
+
+        numpy_requirement = list(parser.parse("numpy == 1.3.0"))[0]
+        self.assertFalse(numpy_requirement.match(Package("numpy", V("1.2.0"))))
+        self.assertTrue(numpy_requirement.match(Package("numpy", V("1.3.0"))))
+        self.assertFalse(numpy_requirement.match(Package("numpy", V("1.4.0"))))
+        self.assertFalse(numpy_requirement.match(Package("numpy", V("1.5.0"))))
