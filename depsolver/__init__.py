fc44763539c751cd1848909890c468ac915ddec5
David Cournapeau <cournape@gmail.com>
2013-03-01 18:59:16 +0000

    ENH: put a few public classes unlikely to change in top namespace.

---
+++ depsolver/__init__.py
@@ -0,0 +1,15 @@
+from depsolver.package \
+    import\
+        Package
+from depsolver.pool \
+    import\
+        Pool
+from depsolver.repository \
+    import\
+        Repository
+from depsolver.requirement \
+    import\
+        Requirement
+from depsolver.solver.core \
+    import\
+        Solver
