bc17ed17e3bfc8fd650691143f1f43c8e006cb71
David Cournapeau <cournape@gmail.com>
2013-03-02 18:49:34 +0000

    REF: use explicit class to track variables.
    Using DecisionSet instead of a simple OrderedDict allows to add more
    info for debugging and diagnostic (such as which clause caused which
    decision to be made).

--- depsolver/solver/core.py
+++ depsolver/solver/core.py
@@ -14,17 +14,55 @@ from depsolver.solver.policy \
         DefaultPolicy
 from depsolver.solver.rule \
     import \
-        Not
+        Not, PackageLiteral
 
-def infer_literal(variables, literal):
+class DecisionsSet(object):
+    def __init__(self, pool):
+        self._data = collections.OrderedDict()
+        self._pool = pool
+        self._decision_map = {}
+
+    def __contains__(self, value):
+        return value in self._data
+
+    def add_decision(self, literal, value, reason):
+        assert literal.name not in self._data
+        self._decision_map[literal.name] = reason
+        self._data[literal.name] = value
+
+    def evaluate_literal(self, literal):
+        return literal.evaluate(literal, self._data)
+
+    def items(self):
+        return iter(self._data.items())
+
+    def is_unit(self, clause):
+        return clause.is_unit(self._data)
+
+    def pop(self):
+        k, v = self._data.pop()
+        del self._decision_map[k]
+        return k, v
+
+    def satisfies_or_none(self, rule):
+        return rule.satisfies_or_none(self._data)
+
+    def __repr__(self):
+        strings = []
+        for pid, v in self._data.items():
+            reason = self._decision_map[pid]
+            strings.append("%s: %s (reason: %s)" % (self._pool.package_by_id(pid), v, reason))
+        return repr(strings)
+
+def infer_literal(variables, literal, reason):
     """Set the literal corresponding variable to the value such as the literal
     is True."""
     if literal.name in variables:
         raise DepSolverError("Internal error: inferring a literal already decided !")
     if isinstance(literal, Not):
-        variables[literal.name] = False
+        variables.add_decision(literal, False, reason)
     else:
-        variables[literal.name] = True
+        variables.add_decision(literal, True, reason)
 
 def run_unit_propagation(clauses, variables):
     """Run unit propagation, i.e. for each unit clause, infer the corresponding
@@ -32,9 +70,9 @@ def run_unit_propagation(clauses, variables):
     """
     iterate_over = clauses[:]
     for clause in iterate_over:
-        is_unit, can_be_infered = clause.is_unit(variables)
+        is_unit, can_be_infered = variables.is_unit(clause)
         if is_unit:
-            infer_literal(variables, can_be_infered)
+            infer_literal(variables, can_be_infered, clause)
             clauses = prune_satisfied_clauses(clauses, variables)
             if clauses is None:
                 raise DepSolverError("Bug in unit propagation ?")
@@ -61,7 +99,7 @@ def prune_satisfied_clauses(clauses, variables):
     new_clauses = []
 
     for clause in clauses:
-        evaluated_or_none = clause.satisfies_or_none(variables)
+        evaluated_or_none = variables.satisfies_or_none(clause)
         if evaluated_or_none is None:
             new_clauses.append(clause)
         elif evaluated_or_none is False:
@@ -75,7 +113,7 @@ def prune_pure_literals(clauses, variables):
         if len(clause.literals) == 1:
             literal = clause.literals[0]
             assert not literal.name in variables
-            infer_literal(variables, literal)
+            infer_literal(variables, literal, clause)
         else:
             new_clauses.append(clause)
 
@@ -96,7 +134,7 @@ def _run_dpll_iteration(clauses, variables):
 def decide_from_assertion_rules(clauses, variables):
     for clause in clauses:
         if clause.is_assertion:
-            infer_literal(variables, clause.get_literal())
+            infer_literal(variables, clause.get_literal(), clause)
 
 class Solver(object):
     def __init__(self, pool, installed_repository, policy=None):
@@ -116,7 +154,7 @@ class Solver(object):
             if len(clauses) == 0:
                 break
             clause = clauses[0]
-            satisfied_or_none = clause.satisfies_or_none(variables)
+            satisfied_or_none = variables.satisfies_or_none(clause)
             if satisfied_or_none is True:
                 clauses = clauses[1:]
                 if len(clauses) < 1:
@@ -133,7 +171,7 @@ class Solver(object):
 
     def _solve_job_clauses(self, clauses, job_clauses, variables):
         for job_clause in job_clauses:
-            is_satisfied_or_none = job_clause.satisfies_or_none(variables)
+            is_satisfied_or_none = variables.satisfies_or_none(job_clause)
 
             if is_satisfied_or_none is True:
                 continue
@@ -170,11 +208,14 @@ class Solver(object):
                 decision_queue)
         while len(candidates) > 0:
             candidate = candidates.popleft()
+            # FIXME: consolidate string vs object for literals
+            l_candidate = PackageLiteral(candidate, self.pool)
             assert not candidate in variables
-            variables[candidate] = True
+            variables.add_decision(l_candidate, True, clause)
             status, new_clauses = _run_dpll_iteration(clauses, variables)
             if status is False:
-                variables[candidate] = False
+                variables.pop()
+                variables.add_decision(l_candidate, False, clause)
                 status, new_clauses = _run_dpll_iteration(clauses, variables)
                 if status is False:
                     # FIXME: refactor solver parts that needs backtracking (and
@@ -202,7 +243,7 @@ class Solver(object):
         clauses = create_install_rules(self.pool, requirement)
         job_clauses = clauses[:1]
 
-        variables = collections.OrderedDict()
+        variables = DecisionsSet(self.pool)
         decide_from_assertion_rules(clauses, variables)
 
         clauses = self._solve_job_clauses(clauses, job_clauses, variables)
